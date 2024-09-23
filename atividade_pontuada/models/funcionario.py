3a89c8f372c195d813752d0488c5d1f0b8ada821
Erick Breno <159458075+derickk0@users.noreply.github.com>
2024-09-23 13:21:56 +0000

    save

---
+++ atividade_pontuada/models/funcionario.py
@@ -0,0 +1,7 @@
+class Funcionario:
+    def __init__(self, cpf:str,rg:str,matricula:) -> None:
+        self.cpf = cpf
+        self.rg = rg
+        self.matricula = matricula
+        self.setor = setor
+        self.salario = salario
